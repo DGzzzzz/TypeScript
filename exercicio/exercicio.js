ccf18715a72e8b560969d5b01d19fbcc0942748a
Douglas Winter <douglaswinter75@gmail.com>
2024-03-04 11:44:44 -0300

    update: exercicio modulo

---
+++ exercicio/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const Multiplica = (num1, num2) => num1 * num2;
+console.log(Multiplica(5, 5));
+const DizOi = (nome) => `Ola ${nome}`;
+console.log(DizOi('Douglas'));
---
+++ exercicio/exercicio.ts
@@ -0,0 +1,5 @@
+const Multiplica = (num1: number, num2: number): number => num1 * num2;
+console.log(Multiplica(5, 5));
+
+const DizOi = (nome: string): string => `Ola ${nome}`
+console.log(DizOi('Douglas'));
